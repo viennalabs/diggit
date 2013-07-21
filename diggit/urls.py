c3b8a9c6bb52f61a91558089cef6a0b861956142
felix <felix@viennalabs.com>
2013-07-21 05:54:04 +0000

    templates

--- diggit/urls.py
+++ diggit/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'diggit.views.home', name='home'),
-    # url(r'^diggit/', include('diggit.foo.urls')),
+from links.views import LinkListView
 
+urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', LinkListView.as_view(), name='home') # ^$ is beginning and end, respectively; that refers to /
+
 )
